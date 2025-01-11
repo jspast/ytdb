c9c1875fe1632870f826b87f15b3597cfc06e59b
jspast <140563347+jspast@users.noreply.github.com>
2025-01-10 21:32:19 -0300

    Fix gatilho.sql

--- db/gatilho.sql
+++ db/gatilho.sql
@@ -1,30 +1,24 @@
--- Procedimento armazenado que ativa o sino de notificações do canal creator
--- para o canal spectator
-CREATE OR REPLACE PROCEDURE turnBellOn(spectator VARCHAR(256), creator VARCHAR(256))
-BEGIN ATOMIC
-    UPDATE subscription SET isbellon = true
-    WHERE spectatorUserName = spectator AND creatorUserName = creator;
-END;
-
-DROP PROCEDURE turnbellon;
-
--- Função equivalente para implementação de gatilho
+-- Função gatilho que ativa o sino de notificações com base em uma visualização
+-- Se o canal que visualizou for inscrito no canal da publicação, o sino ativará
 -- (O PostgreSQL não suporta o uso de procedimentos em gatilhos, apenas funcões.
 --  Ver: https://www.postgresql.org/docs/17/sql-createtrigger.html)
 CREATE OR REPLACE FUNCTION turnBellOn()
 RETURNS TRIGGER AS $$
 BEGIN
     UPDATE subscription SET isbellon = true
-    WHERE spectatorUserName = NEW.spectatorUserName
-    AND creatorUserName = NEW.creatorUserName;
+    WHERE spectatorUserName = NEW.channelusername
+    AND creatorUserName in
+        (SELECT authorusername
+        FROM visualization JOIN POST ON (visualization.POSTID = POST.ID)
+        WHERE POST.ID = NEW.POSTID);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Gatilho que ativa o sino de notificações quando um canal curte uma publicação
 -- de um canal que é inscrito
-CREATE TRIGGER likeTurnsBellOn
-AFTER UPDATE OF liked ON visualization
+CREATE OR REPLACE TRIGGER likeTurnsBellOn
+AFTER INSERT OR UPDATE OF liked ON visualization
 FOR EACH ROW
 WHEN (NEW.liked = true)
 EXECUTE FUNCTION turnBellOn();
